ee6fa2709eaebf4e11808420bf67e8df2f2b4774
alanskimp80 <68741616+alanskimp80@users.noreply.github.com>
2020-08-18 10:24:55 +0100

    Unit Tests

--- CardApp/cardLayoutTests/cardLayoutTests.swift
+++ CardApp/cardLayoutTests/cardLayoutTests.swift
@@ -13,7 +13,7 @@ class cardLayoutTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
     
     override func tearDown() {
@@ -22,7 +22,10 @@ class cardLayoutTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case
+
+        //test to see if at least 1 data point is in the model:
+        XCTAssertEqual(DataModel.sharedInstance.title.count, 1)
+        XCTAssertEqual(DataModel.sharedInstance.description.count, 1)
         
         XCTAssert(true, "All Test Have Passed.")
         
